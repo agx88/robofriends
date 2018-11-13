f98e47c8fecdfc59a703df7a1e1e2df04be298e8
agx88 <peppe-a@live.it>
2018-11-13 14:33:48 +0100

    completed react + redux tutorial

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
-import 'tachyons';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import { searchRobots } from './reducers';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { searchRobots, requestRobots} from './reducers';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 
-const store = createStore(searchRobots)
+//style
+import 'tachyons';
+import './index.css';
+
+const logger = createLogger(); //for logging into application
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
 
 ReactDOM.render(
     <Provider store={store}>
